02d3e407ba9df50d738e7bfb7429d54429d23620
sina khalili <sina.khalili@canalyst.com>
2019-08-21 14:31:58 -0700

    Create base project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/beer_emoji.png">
+    <HelloWorld msg="Welcome to the awesome booze website!!!"/>
+  <api-call/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ApiCall from './components/ApiCall.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ApiCall,
   }
 }
 </script>
@@ -24,5 +27,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  width: 100px;
 }
 </style>
---
+++ src/components/ApiCall.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>==============</p>
+        {{ something }}
+        <p>==============</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+    export default {
+        name: 'api-call',
+        data() {
+            return {
+                something: 'value'
+            }
+        },
+        created() {
+            axios
+            .get('https://b00z3.herokuapp.com/')
+            .then(response => (this.something = response.data))
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
