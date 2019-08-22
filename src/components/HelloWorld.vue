55b5fecc98663a2b39eaeb0c137dc76ffee7ecde
sina khalili <sina.khalili@canalyst.com>
2019-08-22 01:52:33 -0700

    Add website functionality to read from array

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p> Here is where you would put in your info!!! </p>
-    <p> Hello world wide web from Vancouver</p>
-  </div>
+  <el-row type="flex" class="row-bg" justify="center">
+    <el-col :span="6">
+      <el-container>
+          <h1>Ready to b00z3?</h1>
+          <el-image style="width: 100px; height: 100px" :src="url" :fit="fit"></el-image>
+      </el-container>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        test: '../assets/logo.png',
+        fit: 'contain',
+      }
+    },
+    computed: {
+      url() {
+        return require('../assets/logo.png')
+      }
+    },
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
---
+++ src/components/Team.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <el-row>
+            <el-col>
+                <div class="helptext"> To get started, click the button below and add member to your party </div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :xs="12" :sm="6" :md="4" :lg="3" :xl="1" v-for="person in people" :key="person.id">
+                <el-card class="box-card">
+                    <div>Age: {{person.age}}</div>
+                    <div>Married? {{person.married}}</div>
+                    <div>Gender: {{person.gender}}</div>
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-button class='give-margin' type="primary" icon="el-icon-circle-plus" circle></el-button>
+        <el-button class='give-margin' type="success" round>Analyze!</el-button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'team',
+        data() {
+            return {
+                people: [{
+                    'age': 21,
+                    'married': false,
+                    'gender': 'male',
+                },
+                {
+                    'age': 21,
+                    'married': false,
+                    'gender': 'male',
+                },
+                {
+                    'age': 21,
+                    'married': false,
+                    'gender': 'male',
+                },
+                {
+                    'age': 21,
+                    'married': false,
+                    'gender': 'male',
+                }]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .helptext {
+        margin: 3em
+    }
+      .text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
+
+  .box-card {
+    margin: 1%;
+  }
+  .give-margin {
+      margin:1em
+  }
+</style>
